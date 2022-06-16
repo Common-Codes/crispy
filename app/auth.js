85f1de5dede6916e3440e119dfc0c8897593827a
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-06-16 18:09:29 +0200

    Fix everytong

--- app/auth.js
+++ app/auth.js
@@ -1,58 +1,60 @@
 auth.onAuthStateChanged(user => {
-    if (user) {
-      store.collection('guilds').get().then(snapshot => {
-        setupGuilds(snapshot.docs);
-        setupUI(user);
-        setupBadges(user);
-      }, err => {
-        window.alert(err.message)
-      });
-    } else {
-      setupGuilds([])
-      setupUI();
-    }
-  })
+  if (user) {
+    store.collection('guilds').get().then(snapshot => {
+      setupGuilds(snapshot.docs, user);
+      setupUI(user);
+      setupBadges(user);
+    }, err => {
+      window.alert(err.message)
+    });
+  } else {
+    setupGuilds([])
+    setupUI();
+    document.getElementById('guild-title-display').innerText = 'You Must First Log In To Access Crispy'
+  }
+})
 
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  const email = signupForm['signup-email'].value;
-  const password = signupForm['signup-password'].value;
+e.preventDefault();
 
-  auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    return store.collection('users').doc(cred.user.uid).set({
-      name: signupForm['signup-name'].value,
-      email: signupForm['signup-email'].value
-    });
-  }).then(() => {
-    //Send optional email verification
-    firebase.auth().currentUser.sendEmailVerification();
-    const modal = document.querySelector('#modal-signup');
-    M.Modal.getInstance(modal).close();
-    signupForm.reset();
+const email = signupForm['signup-email'].value;
+const password = signupForm['signup-password'].value;
+
+auth.createUserWithEmailAndPassword(email, password).then(cred => {
+  return store.collection('users').doc(cred.user.uid).set({
+    name: signupForm['signup-name'].value,
+    email: signupForm['signup-email'].value
   });
+}).then(() => {
+  //making people verify emails... maybe...
+  firebase.auth().currentUser.sendEmailVerification();
+  //ok, not quite there yet...
+  const modal = document.querySelector('#modal-signup');
+  M.Modal.getInstance(modal).close();
+  signupForm.reset();
+});
 });
 
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
-  e.preventDefault();
-  auth.signOut().then(() => {
-    document.location.href = 'https://common-codes.github.io/crispy/';
-  })
+e.preventDefault();
+auth.signOut().then(() => {
+  document.location.href = 'https://common-codes.github.io/crispy/';
+})
 });
 
 const loginForm = document.querySelector('#login-form');
 loginForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  const email = loginForm['login-email'].value;
-  const password = loginForm['login-password'].value;
+e.preventDefault();
 
-  auth.signInWithEmailAndPassword(email, password).then((cred) => {
-    const modal = document.querySelector('#modal-login');
-    M.Modal.getInstance(modal).close();
-    loginForm.reset();
-  });
+const email = loginForm['login-email'].value;
+const password = loginForm['login-password'].value;
+
+auth.signInWithEmailAndPassword(email, password).then((cred) => {
+  const modal = document.querySelector('#modal-login');
+  M.Modal.getInstance(modal).close();
+  loginForm.reset();
+});
 
 });
--- app/index.js
+++ app/index.js
@@ -4,33 +4,49 @@ const loggedOutLinks = document.querySelectorAll('.logged-out');
 const loggedInLinks = document.querySelectorAll('.logged-in');
 const accountDetails = document.querySelector('.account-details');
 const accountBadges = document.querySelector('.account-badges');
-const currentGuildTitleDisplay = document.getElementById("guild-title-current");
-const temporary = document.getElementById("guild-context");
+const currentGuildDisplay = document.getElementById('chat_inner_container');
+let nameVar = ''
+let guildVar = ''
+let profileVar = ''
+
+const validURL = (str) =>{
+  var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+  if(!regex.test(str)){
+      return false; //false
+  } else {
+      return true; //true
+  }
+}
 
 const setupBadges = (user) => {
   if(user){
     store.collection('users').doc(user.uid).get().then(doc => {
-      const html = `
+      const badges = doc.data(); 
+      const badge = `
       <div class="_category_sso5v_101">Badges</div>
       <div class="modal-content UserBadges__BadgesBase-sc-1ubexy3-0 gMJfVq" style="display: flex;">
         <div style="display: flex;">
-          <img src="https://www.svgrepo.com/show/182103/badge-medal.svg" style="height: 26px; width: 26px;">
+          <img title="Kickstarter" src="https://www.svgrepo.com/show/182103/badge-medal.svg" style="height: 28px; width: 28px;">
         </div>
-        <div style="display: none;">
-          <img src="https://www.svgrepo.com/show/193147/badge.svg" style="height: 26px; width: 26px;">
+        <div style="display: flex;">
+          <img title="Bug Hunter" src="https://www.svgrepo.com/show/193147/badge.svg" style="height: 28px; width: 28px;">
         </div>
       </div>
       `;
-      accountBadges.innerHTML = html;
+      accountBadges.innerHTML = badge;
     })
+  } else{
+    accountBadges.innerHTML = ''
   }
 }
 
 const setupUI = (user) => {
   if(user){
     store.collection('users').doc(user.uid).get().then(doc => {
+      nameVar = `${doc.data().name}`
+      profileVar = `${doc.data().img}`
       const html = `
-        <div><img src="https://ggpht.ga/icons/${doc.data().img}?size=full" style="height: 48px; width: 48px;"></div>
+        <div><img src="${doc.data().img}" title="Profile" style="height: 48px; width: 48px;"></div>
         <div>Logged in as ${doc.data().name}</div>
         <div>${doc.data().email}</div>
       `;
@@ -54,7 +70,7 @@ const setupGuilds = (data) => {
       const guild = doc.data();
       const li = `
         <li>
-          <div> <button alt="${guild.title}" title="${guild.title}" style="transform: rotate(-90deg);" onclick="${guild.display}"><img alt="${guild.title}" src="http://ggpht.ga/icons/${guild.img}?size=full" style="width: 48px; height: 48px;"></button> </div>
+          <div> <button alt="${guild.title}" title="${guild.title}" style="transform: rotate(-90deg);" onclick="${guild.display}"><img alt="${guild.title}" src="${guild.img}" style="width: 48px; height: 48px;"></button> </div>
         </li>
       `;
       html += li;
@@ -67,22 +83,258 @@ const setupGuilds = (data) => {
   }
 };
 
-const displayActiveChat = () => {
-  html = `<h4 style="color: #151515;">Active Guild</h4>`
-  currentGuildTitleDisplay.innerHTML = html
-  temporary.innerHTML = '<p style="color: #151515;">V1RU5: Trest</p>'
+const create_load = (id) =>{
+  var parent = this;
+  var container = document.getElementById(id)
+  container.innerHTML = ''
+
+  var loader_container = document.createElement('div')
+  loader_container.setAttribute('class', 'loader_container')
+
+  var loader = document.createElement('div')
+  loader.setAttribute('class', 'loader')
+
+  loader_container.append(loader)
+  container.append(loader_container)
+
 }
+
+const send_message = (message) => {
+  var parent = this
+
+  var ref = firebase.database().ref();
+  
+  if(nameVar == null && message == null){
+    return
+  }
+
+  var messages = db.ref(`chats/${guildVar}`);
+  messages.once('value', function(snapshot) {
+    var index = parseFloat(snapshot.numChildren()) + 1
+    db.ref('chats/' + `${guildVar}` + `/message_${index}`).set({
+      name: nameVar,
+      message: message,
+      avatar: profileVar,
+      index: index
+    })
+    .then(function(){
+      refresh_chat()
+    })
+  })
+}
+
+const displayActiveChat = () => {
+  guildVar = 'creak'
+  document.getElementById('guild-title-display').innerText = 'TallerThanShort\'s Crack Den'
+    html = 
+    `
+    <div id="chat_input_container">
+      <input type="text" id="chat_input" />
+      <label for="chat_input">Send Message</label>
+      <div id="chat_input_send">
+        <button id="chat_input_send" style="display: none;">
+      </div>
+    </div>
+    `
+    currentGuildDisplay.innerHTML += html
+    const chat_input = document.getElementById('chat_input');
+    const chat_input_send = document.getElementById('chat_input_send')
+    var messages = db.ref(`chats/${guildVar}`);
+    chat_input.onkeyup  = function(){
+      if(chat_input.value.length > 0){
+        if (event.keyCode === 13) {
+          document.getElementById("chat_input_send").click();
+         }
+        chat_input_send.removeAttribute('disabled')
+        chat_input_send.classList.add('enabled')
+        chat_input_send.onclick = function(){
+          chat_input_send.setAttribute('disabled', true)
+          chat_input_send.classList.remove('enabled')
+          if(chat_input.value.length <= 0){
+            return
+          }
+          create_load('chat_content_container')
+          send_message(chat_input.value)
+          chat_input.value = ''
+          // Focus on the input there after
+          chat_input.focus()
+        }
+      }else{
+        chat_input_send.classList.remove('enabled')
+      }
+    }
+
+
+
+
+    document.getElementById('chat_content_container').innerHTML = `<p style="style="color: #151515;">${nameVar}, Say Something</p>`
+};
 
 const displayNotifChat = () => {
-html = `<h4 style="color: #151515;">Notifications Guild</h4>`
-currentGuildTitleDisplay.innerHTML = html // THIS IS ALL TEMPORARY
-temporary.innerHTML = '<p style="color: #151515;">Crispy Admin: Mobile Support Pushed!</p>'
-}
+  guildVar = 'common'
+  document.getElementById('guild-title-display').innerText = 'Common-Codes'
+    html = 
+    `
+    <div id="chat_input_container">
+      <input type="text" id="chat_input" />
+      <label for="chat_input">Send Message</label>
+      <div id="chat_input_send">
+        <button id="chat_input_send" style="display: none;">
+      </div>
+    </div>
+    `
+    currentGuildDisplay.innerHTML += html
+    const chat_input = document.getElementById('chat_input');
+    const chat_input_send = document.getElementById('chat_input_send')
+    var messages = db.ref(`chats/${guildVar}`);
+    chat_input.onkeyup  = function(){
+      if(chat_input.value.length > 0){
+        if (event.keyCode === 13) {
+          document.getElementById("chat_input_send").click();
+         }
+        chat_input_send.removeAttribute('disabled')
+        chat_input_send.classList.add('enabled')
+        chat_input_send.onclick = function(){
+          chat_input_send.setAttribute('disabled', true)
+          chat_input_send.classList.remove('enabled')
+          if(chat_input.value.length <= 0){
+            return
+          }
+          create_load('chat_content_container')
+          send_message(chat_input.value)
+          chat_input.value = ''
+          // Focus on the input there after
+          chat_input.focus()
+        }
+      }else{
+        chat_input_send.classList.remove('enabled')
+      }
+    }
+
+
+
+
+    document.getElementById('chat_content_container').innerHTML = `<p style="style="color: #151515;">${nameVar}, Say Something</p>`
+};
 
 const displayNewGuild = () => {
-html = `<h4 style="color: #151515;">VTC - Very Terrible Code</h4>`
-currentGuildTitleDisplay.innerHTML = html
-temporary.innerHTML = `<p style="color: #151515;">this was approved by the devs @ VTC</p>`
+  guildVar = 'coolkidsclub'
+  document.getElementById('guild-title-display').innerText = 'VTC - Very Terrible Code'
+    html = 
+    `
+    <div id="chat_input_container">
+      <input type="text" id="chat_input" />
+      <label for="chat_input">Send Message</label>
+      <div id="chat_input_send">
+        <button id="chat_input_send" style="display: none;">
+      </div>
+    </div>
+    `
+    currentGuildDisplay.innerHTML += html
+    const chat_input = document.getElementById('chat_input');
+    const chat_input_send = document.getElementById('chat_input_send')
+    var messages = db.ref(`chats/${guildVar}`);
+    chat_input.onkeyup  = function(){
+      if(chat_input.value.length > 0){
+        if (event.keyCode === 13) {
+          document.getElementById("chat_input_send").click();
+         }
+        chat_input_send.removeAttribute('disabled')
+        chat_input_send.classList.add('enabled')
+        chat_input_send.onclick = function(){
+          chat_input_send.setAttribute('disabled', true)
+          chat_input_send.classList.remove('enabled')
+          if(chat_input.value.length <= 0){
+            return
+          }
+          create_load('chat_content_container')
+          send_message(chat_input.value)
+          chat_input.value = ''
+          // Focus on the input there after
+          chat_input.focus()
+        }
+      }else{
+        chat_input_send.classList.remove('enabled')
+      }
+    }
+
+
+
+
+    document.getElementById('chat_content_container').innerHTML = `<p style="style="color: #151515;">${nameVar}, Say Something</p>`
+};
+
+const refresh_chat = () => {
+  var chat_content_container = document.getElementById('chat_content_container')
+
+  var messages = db.ref(`chats/${guildVar}`);
+  messages.on('value', function(snapshot) {
+    chat_content_container.innerHTML = ''
+    if(snapshot.numChildren() == 0){
+      return
+    }
+    var values = Object.values(snapshot.val());
+    var guide = []
+    var unordered = []
+    var ordered = []
+    for (var i, i = 0; i < values.length; i++) {
+      guide.push(i+1)
+      unordered.push([values[i], values[i].index]);
+    }
+
+    guide.forEach(function(key) {
+      var found = false
+      unordered = unordered.filter(function(item) {
+        if(!found && item[1] == key) {
+          ordered.push(item[0])
+          found = true
+          return false
+        }else{
+          return true
+        }
+      })
+    })
+
+    ordered.forEach(function(data) {
+      var name = data.name
+      var avatar = data.avatar
+      var message = data.message
+
+      var message_container = document.createElement('div')
+      message_container.setAttribute('class', 'message_container')
+
+      var message_inner_container = document.createElement('div')
+      message_inner_container.setAttribute('class', 'message_inner_container')
+
+      var message_user_container = document.createElement('div')
+      message_user_container.setAttribute('class', 'message_user_container')
+
+      var message_user = document.createElement('div')
+      message_user.setAttribute('class', 'message_user')
+      message_user.innerHTML = `<img src="${avatar}" style="width: 18px; height: 18px; border-radius: 50%;"> ${name}`
+
+      var message_content_container = document.createElement('div')
+      message_content_container.setAttribute('class', 'message_content_container')
+
+      if(validURL(message)){
+        var message_content = document.createElement('div')
+        message_content.setAttribute('class', 'message_content')
+        message_content.innerHTML = `<p style="text-decoration: underline; cursor: pointer;" onclick="location.href='${message}'">${message}</a>\n<div class="message_embed"><img src="${message}" style="cursor: default;"></div>`;
+      } else{
+        var message_content = document.createElement('p')
+        message_content.setAttribute('class', 'message_content')
+        message_content.textContent = `${message}`;
+      }
+
+      message_user_container.append(message_user)
+      message_content_container.append(message_content)
+      message_inner_container.append(message_user_container, message_content_container)
+      message_container.append(message_inner_container)
+
+      chat_content_container.append(message_container)
+    });
+    chat_content_container.scrollTop = chat_content_container.scrollHeight;
+  })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
