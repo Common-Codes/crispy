40a0d095638d9e4b4d31a152a9ab710df7916d47
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-10-29 19:37:42 +0200

    Fix: Clear badges on `ext-badge-location`

--- app/profiles.js
+++ app/profiles.js
@@ -5,6 +5,7 @@ const userBadges = document.getElementById('ext-badge-location');
 document.addEventListener('click', (e) => {
     let element = e.target;
     if(element.tagName == "USERNAME"){
+        userBadges.innerHTML = '';
         store.collection("users").where("name", "==", element.innerText).get().then((querySnaphot) => {
             querySnaphot.forEach((doc) => {
                 const usera = doc.data();
@@ -14,7 +15,6 @@ document.addEventListener('click', (e) => {
                 <div><img src="${usera.img}" title="${usera.name}'s profile image" style="height: 48px; width: 48px; border-radius: 50%;"></div>
                 <div>Username: ${usera.name}</div>
                 <hr>
-                <b style="color: crimson">BUG! Use the button below to exit, otherwise badge data may get duplicated.</b><br>
                 <button class="btn yellow darken-2 z-depth-0" onclick="closeModal()">Close</button>
                 `;
                 store.collection("users").doc(userTag).collection("badges").get().then(doc => {
